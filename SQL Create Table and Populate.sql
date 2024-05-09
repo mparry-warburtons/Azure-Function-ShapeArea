4b0b83743e223d211f57cfbd28ec4e3f92b0c246
mparry-warburtons <martin.parry@warburtons.co.uk>
2024-05-09 15:21:38 +0100

    SQL script tweaks , deployment profile created

--- SQL Create Table and Populate.sql
+++ SQL Create Table and Populate.sql
@@ -1,23 +1,23 @@
 
 --drop  table example.RawShapes
 
-create table example.RawShapes
+create table example.Shapes
 (
 	Id int identity(1,1),
 	ShapeName nvarchar(50) not null,
-	Dimensions nvarchar(50) null,
+	Dimensions nvarchar(50) null default 'N/A',
 	Units nvarchar(50) not null,
-	Radius decimal(5,3),
+	Radius decimal(5,3) default 0,
 	Area decimal(10,5)
 )
 
 
-insert into example.RawShapes
+insert into example.Shapes
 ( ShapeName,Dimensions,Units,Radius, Area)
 
-select 'Rectangle' , '5x5' , 'cm' , null , null  union all 
-select 'Triangle' , '5x4x3' , 'cm' , null  , null    union all 
-select 'Circle' , null , 'cm' , 4.4 , null 
+select 'Rectangle' , '5x5' , 'cm' , 0 , null  union all 
+select 'Triangle' , '5x4x3' , 'cm' , 0  , null    union all 
+select 'Circle' , 'N/A' , 'cm' , 4.4 , null 
 
 
-select * from example.RawShapes
+select * from example.Shapes
